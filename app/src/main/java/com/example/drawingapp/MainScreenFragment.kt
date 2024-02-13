24f982e67d00f79f532451b39daed8d6f0e6f252
ethanbowles <cottonwoodcoyoteutah@gmail.com>
2024-02-12 19:47:25 -0700

    Renamed incorrect file name

--- app/src/main/java/com/example/drawingapp/MainScreenFragment.kt
+++ app/src/main/java/com/example/drawingapp/MainScreenFragment.kt
@@ -5,10 +5,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.drawingapp.databinding.FragmentDrawingRecyclerBinding
+import com.example.drawingapp.databinding.FragmentMainScreenBinding
 
 class MainScreenFragment : Fragment() {
-    private lateinit var binding: FragmentDrawingRecyclerBinding
+    private lateinit var binding: FragmentMainScreenBinding
     private var clickCallback : () -> Unit = {}
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
