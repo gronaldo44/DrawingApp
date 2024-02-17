a8feec025d244d05af4c63f5950af8b797d84369
ethanbowles <cottonwoodcoyoteutah@gmail.com>
2024-02-16 17:58:41 -0700

    Commenting

--- app/src/main/java/com/example/drawingapp/SplashScreenFragment.kt
+++ app/src/main/java/com/example/drawingapp/SplashScreenFragment.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.FrameLayout
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.example.drawingapp.databinding.FragmentSplashScreenBinding
@@ -41,6 +42,5 @@ class SplashScreenFragment : Fragment() {
         binding.continueButton.setOnClickListener {
             findNavController().navigate(R.id.closedSplashScreen)
         }
-        return binding.root
     }
 }
