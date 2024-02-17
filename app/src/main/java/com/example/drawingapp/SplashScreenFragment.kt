f50ff87f2a3d7895aa0a7c6621c09e323e7f46c2
Ronald Foster <gronaldo44@gmail.com>
2024-02-16 17:51:23 -0700

    Made SplashScreenFragment.kt dynamically switch between landscape and portrait layouts based on orientation.

--- app/src/main/java/com/example/drawingapp/SplashScreenFragment.kt
+++ app/src/main/java/com/example/drawingapp/SplashScreenFragment.kt
@@ -1,26 +1,38 @@
 package com.example.drawingapp
 
 import android.annotation.SuppressLint
+import android.content.res.Configuration
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.example.drawingapp.databinding.FragmentSplashScreenBinding
 
 @SuppressLint("CustomSplashScreen")
 class SplashScreenFragment : Fragment() {
     private lateinit var binding: FragmentSplashScreenBinding
-    private var clickCallback : () -> Unit = {}
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        binding = FragmentSplashScreenBinding.inflate(layoutInflater,  container, false)
+    ): View {
+        // Inflate the appropriate layout based on screen orientation
+        return if (resources.configuration.orientation == Configuration.ORIENTATION_LANDSCAPE) {
+            inflater.inflate(R.layout.fragment_splash_screen_land, container, false)
+        } else {
+            inflater.inflate(R.layout.fragment_splash_screen, container, false)
+        }
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        // Find views and set click listeners as necessary
+        binding = FragmentSplashScreenBinding.bind(view)
         binding.continueButton.setOnClickListener {
             findNavController().navigate(R.id.closedSplashScreen)
         }
-        return binding.root
     }
 }
