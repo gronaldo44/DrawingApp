6a52e028493b13bdcfb6696147928855fa35e390
ethanbowles <cottonwoodcoyoteutah@gmail.com>
2024-03-20 02:26:49 -0600

    Jetpack composed splash screen

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,6 +15,9 @@ android {
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        vectorDrawables {
+            useSupportLibrary = true
+        }
     }
 
     buildTypes {
@@ -33,9 +36,21 @@ android {
     buildFeatures{
         viewBinding = true
         dataBinding = true
+        compose = true
     }
     kotlinOptions {
         jvmTarget = "1.8"
+    }
+    buildFeatures {
+        compose = true
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.5.9"
+    }
+    packaging {
+        resources {
+            excludes += "/META-INF/{AL2.0,LGPL2.1}"
+        }
     }
 }
 
@@ -44,6 +59,11 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation(platform("androidx.compose:compose-bom:2023.08.00"))
+    implementation("androidx.compose.ui:ui")
+    implementation("androidx.compose.ui:ui-graphics")
+    implementation("androidx.compose.ui:ui-tooling-preview")
+    implementation("androidx.compose.material3:material3")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
@@ -52,9 +72,29 @@ dependencies {
     implementation("androidx.activity:activity-ktx:1.8.2")
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.7.0")
     implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.7.0")
-    implementation("androidx.navigation:navigation-fragment-ktx:2.7.6")
+    implementation("androidx.navigation:navigation-fragment-ktx:2.7.7")
     androidTestImplementation("androidx.arch.core:core-testing:+")
 
     // Color wheel selector
    implementation("com.github.QuadFlask:colorpicker:0.0.13")
+
+    //Compose
+    implementation("androidx.core:core-ktx:1.12.0")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
+    implementation("androidx.activity:activity-compose:1.8.2")
+    implementation(platform("androidx.compose:compose-bom:2024.02.02"))
+    implementation("com.google.android.material:material:1.11.0")
+    implementation("androidx.navigation:navigation-common-ktx:2.7.7")
+    implementation("androidx.navigation:navigation-fragment-ktx:2.7.7")
+
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.7.0")
+
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    androidTestImplementation(platform("androidx.compose:compose-bom:2024.02.02"))
+    androidTestImplementation("androidx.compose.ui:ui-test-junit4")
+    androidTestImplementation(platform("androidx.compose:compose-bom:2023.08.00"))
+    debugImplementation("androidx.compose.ui:ui-tooling")
+    debugImplementation("androidx.compose.ui:ui-test-manifest")
 }
--- app/src/main/java/com/example/drawingapp/SplashScreenFragment.kt
+++ app/src/main/java/com/example/drawingapp/SplashScreenFragment.kt
@@ -1,13 +1,31 @@
 package com.example.drawingapp
 
 import android.annotation.SuppressLint
+import android.content.res.Configuration
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.navigation.fragment.findNavController
 import com.example.drawingapp.databinding.FragmentSplashScreenBinding
+import androidx.compose.material3.Text
+import androidx.compose.material3.Button
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.drawingapp.ui.theme.DrawingAppTheme
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.sp
 
 /**
  * Splash Screen Fragment class. Holds a splash screen with a simple button that moves to the
@@ -16,19 +34,127 @@ import com.example.drawingapp.databinding.FragmentSplashScreenBinding
 @SuppressLint("CustomSplashScreen")
 class SplashScreenFragment : Fragment() {
     private lateinit var binding: FragmentSplashScreenBinding
-    private var clickCallback : () -> Unit = {}
 
     /**
      * Displays the fragment and adds a listener to the button to continue to the home screen
      */
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         binding = FragmentSplashScreenBinding.inflate(layoutInflater,  container, false)
-        binding.continueButton.setOnClickListener {
-            findNavController().navigate(R.id.closedSplashScreen)
+        binding.composeSplashScreen?.setContent {
+            val configuration = LocalConfiguration.current
+            when (configuration.orientation) {
+                Configuration.ORIENTATION_LANDSCAPE -> {
+                    MyComposableLand(Modifier.padding(16.dp)){
+                        findNavController().navigate(R.id.closedSplashScreen)
+                    }
+                }
+                else -> {
+                    MyComposablePort(Modifier.padding(16.dp)){
+                        findNavController().navigate(R.id.closedSplashScreen)
+                    }
+                }
+            }
         }
+
         return binding.root
+    }
+}
+@Composable
+fun MyComposablePort(modifier: Modifier = Modifier,
+                 onClick: ()->Unit){
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+
+        Image(
+            painter = painterResource(id = R.drawable.paintlogo),
+            contentDescription = "App Logo",
+            modifier = Modifier
+                .fillMaxWidth(0.4f)
+        )
+
+        Text(
+            text = "Welcome To Our Drawing App",
+            modifier = Modifier
+                .fillMaxWidth(),
+            fontSize = 28.sp,
+            fontWeight = FontWeight.Bold,
+            textAlign = TextAlign.Center
+        )
+
+        Text(
+            text = "In this app you can create drawings through a paint type editor. You can add, edit, and remove such drawings. Please continue to the next screen in order to create a drawing!",
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(10.dp),
+            fontSize = 16.sp,
+            textAlign = TextAlign.Center
+        )
+
+        Button(
+            onClick = onClick,
+            modifier = Modifier.fillMaxWidth(0.5f)
+        ) {
+            Text(text = "Continue To App")
+        }
+    }
+}
+
+@Composable
+fun MyComposableLand(modifier: Modifier = Modifier,
+                     onClick: ()->Unit){
+    Row(modifier = Modifier.fillMaxSize()) {
+        Column(
+            modifier = Modifier
+                .weight(1f)
+                .fillMaxHeight()
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.paintlogo),
+                contentDescription = "App Logo",
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(top = 16.dp),
+            )
+
+            Button(
+                onClick = onClick,
+                modifier = Modifier
+                    .fillMaxWidth()
+            ) {
+                Text("Continue To App")
+            }
+        }
+
+        // Container for welcome message and content message (2/3 of width)
+        Column(
+            modifier = Modifier
+                .weight(2f)
+                .fillMaxHeight()
+        ) {
+            Text(
+                text = "Welcome To Our Drawing App",
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(end = 16.dp, top = 50.dp),
+                textAlign = TextAlign.Center,
+                fontSize = 48.sp,
+                fontWeight = FontWeight.Bold
+            )
+
+            Text(
+                text = "In this app you can create drawings through a paint type editor. You can add, edit, and remove such drawings. Please continue to the next screen in order to create a drawing!",
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(start = 16.dp, end = 16.dp, top = 8.dp),
+                fontSize = 26.sp
+            )
+        }
     }
 }
